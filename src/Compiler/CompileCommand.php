68fc37e31b7e9cc1e8a23d5a64fdc01dc7af0836
Lukas Klima <arziel12@gmail.com>
2018-01-17 10:47:22 +0100

    posun

--- src/Compiler/CompileCommand.php
+++ src/Compiler/CompileCommand.php
@@ -2,6 +2,7 @@
 
 namespace PiCompiler\Compiler;
 
+use Nette\Utils\Finder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -9,35 +10,54 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class CompileCommand extends Command
 {
-	private const ARG_FILES = 'files';
+	public const COMMAND_NAME = 'compile';
+	private const ARG_FILES = 'file';
+	private const REQUIRED_ARRAY = InputArgument::IS_ARRAY | InputArgument::REQUIRED;
 	/** @var \PiCompiler\Compiler\Configuration */
 	private $configuration;
-	
 	private $files;
 	
+	/**
+	 *
+	 * @throws \Symfony\Component\Console\Exception\LogicException
+	 */
 	public function __construct(\PiCompiler\Compiler\Configuration $configuration)
 	{
-		$this->setName('compile');
-		$this->setDescription('Compile files');
-		$this->addArgument(self::ARG_FILES, InputArgument::IS_ARRAY, 'Files to compile');
-		
-		parent::__construct();
+		parent::__construct(self::COMMAND_NAME);
 		
 		$this->configuration = $configuration;
 		
-		
+		$this->addArgument(self::ARG_FILES, self::REQUIRED_ARRAY, '', []);
+		$this->setDescription('Compile files');
 	}
 	
+	protected function initialize(InputInterface $input, OutputInterface $output)
+	{
+		parent::initialize($input, $output);
+		
+		$this->files = $input->getArgument(self::ARG_FILES);
+		
+		if ($this->files === []) {
+			// TODO hezkou message, kdyz chybi files
+			die('Missing file.');
+		}
+	}
 	
-	public function run(InputInterface $input, OutputInterface $output)
+	public function execute(InputInterface $input, OutputInterface $output)
 	{
 		
 		\dump('-------------');
+
+//		dump($this->configuration);
+//		dump($this->configuration->getDefaultPreset());
 		
-		dump($this->configuration);
-		dump($this->configuration->getDefaultPreset());
-		
-		\dump($input);
-		\dump($input->getArgument(self::ARG_FILES));
+		Finder::findFiles('*.phpp')
+	
+	
+	}
+	
+	public function getFiles(): array
+	{
+		return $this->files;
 	}
 }
